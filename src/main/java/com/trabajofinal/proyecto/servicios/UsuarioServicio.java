3189218586c9293a7cc0cf563a0fb04dec732167
vicmusic <vicmusic22@gmail.com>
2021-08-11 20:14:09 -0300

    UsuarioServicio
    Terminada

--- src/main/java/com/trabajofinal/proyecto/servicios/UsuarioServicio.java
+++ src/main/java/com/trabajofinal/proyecto/servicios/UsuarioServicio.java
@@ -24,9 +24,10 @@ public class UsuarioServicio {
     private UsuarioRepositorio usuariorepositorio;
 
     @Transactional(propagation = Propagation.REQUIRED, rollbackFor = {Exception.class})
-    public Usuario guardarUsuario(String rol, String email, String clave, Date creado, Date editado, boolean activo) throws Exception 
-    {
-
+    public Usuario guardarUsuario(String rol, String email, String clave, Date creado, Date editado, boolean activo) throws Exception {
+        
+        validar(rol, email, clave, creado, editado, activo);
+        
         Usuario usuarioG = new Usuario();
 
         usuarioG.setRol(Rol.valueOf(clave));
@@ -39,34 +40,55 @@ public class UsuarioServicio {
 
         return usuariorepositorio.save(usuarioG);
     }
-    
-    
-    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = { Exception.class })
-    public Usuario altaUsuario(String id){
-        
+
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = {Exception.class})
+    public Usuario altaUsuario(String id) {
+
         Usuario usuarioA = usuariorepositorio.getOne(id);
         usuarioA.setActivo(true);
         return usuariorepositorio.save(usuarioA);
     }
-    
-    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = { Exception.class })
-	public Usuario bajaUsuario(String id) {
 
-		Usuario usuarioB = usuariorepositorio.getOne(id);
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = {Exception.class})
+    public Usuario bajaUsuario(String id) {
 
-		usuarioB.setActivo(false);
-		return usuariorepositorio.save(usuarioB);
-	}
-        
-        // Metodo listar
-        @Transactional(readOnly = true)
-	public List<Usuario> usuarioActivos() {
-		return usuariorepositorio.buscarUsuario();
-                
-	}
-        
-        
-    
-    
+        Usuario usuarioB = usuariorepositorio.getOne(id);
+
+        usuarioB.setActivo(false);
+        return usuariorepositorio.save(usuarioB);
+    }
+
+    // Metodo listar
+    @Transactional(readOnly = true)
+    public List<Usuario> usuarioActivos() {
+        return usuariorepositorio.buscarUsuario();
+
+    }
+
+    public List<Usuario> listarTodo() {
+        return usuariorepositorio.findAll();
+    }
+
+    public void validar(String rol, String email, String clave, Date creado, Date editado, boolean activo) throws Exception {
+        if (rol == null || rol.isEmpty() || rol.contains("  ")) {
+            throw new Exception();
+        }
+
+        if (email == null || email.isEmpty() || email.contains("  ")) {
+            throw new Exception();
+        }
+
+        if (clave == null || clave.isEmpty() || clave.contains("  ")) {
+            throw new Exception();
+        }
+
+        if (creado == null) {
+            throw new Exception();
+        }
+        if (editado == null) {
+            throw new Exception();
+        }
+
+    }
 
 }
