38f936d3dcb72ca41e722f637e45016af7b565c2
vicmusic <vicmusic22@gmail.com>
2021-08-10 22:40:01 -0300

    ReiniciarGit
    Cargar Class

--- src/main/java/com/trabajofinal/proyecto/repositorios/SocioRepositorio.java
+++ src/main/java/com/trabajofinal/proyecto/repositorios/SocioRepositorio.java
@@ -4,19 +4,15 @@
 package com.trabajofinal.proyecto.repositorios;
 
 import com.trabajofinal.proyecto.entidades.Socio;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 
 /**
  *
  * @author VicMusic
  */
 @Repository
-public interface SocioRepositorio extends JpaRepository<Socio, Integer> {
-
-    @Query("SELECT a from socio a WHERE a.activo = true ")
-    public List<Socio> buscarSocio();
-
+public interface SocioRepositorio extends JpaRepository<Socio, Integer>{
+    
 }
