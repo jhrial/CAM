be849674f0d142a25a4a37a34f0f948055a24778
vicmusic <vicmusic22@gmail.com>
2021-08-10 22:45:02 -0300

    Class Servicio
    Creación

---
+++ src/main/java/com/trabajofinal/proyecto/servicios/UsuarioServicio.java
@@ -0,0 +1,15 @@
+/*
+ 
+ */
+package com.trabajofinal.proyecto.servicios;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author VicMusic
+ */
+@Service
+public class UsuarioServicio {
+    
+}
