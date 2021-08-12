4e6b617b43ddbb216ec6be8909ae712c58ef53b4
vicmusic <vicmusic22@gmail.com>
2021-08-11 22:27:01 -0300

    Create PagoServicio.java

---
+++ src/main/java/com/trabajofinal/proyecto/servicios/PagoServicio.java
@@ -0,0 +1,19 @@
+/*
+ Actividades que se realizan al hacer un pago
+
+preguntas
+
+se paga una membresia y que pasa con las actividades.
+que sucede con los socio deudor
+
+
+ */
+package com.trabajofinal.proyecto.servicios;
+
+/**
+ *
+ * @author VicMusic
+ */
+public class PagoServicio {
+    
+}
